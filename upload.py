fb743dcfdfea1a5e01ea90f179498e5420a59f4a
elden <mcgods@sina.com>
2025-05-31 11:38:12 +0800

    只生成对应相册的index.md与压缩图片, 不再自动上传ftp或者git

--- upload.py
+++ upload.py
@@ -4,6 +4,7 @@ from multiprocessing import Pool
 from upload_utils import *
 
 if __name__ == "__main__":
+    total_time = time.time()
     with open("config.json", "r", encoding='utf-8') as f:
         config = json.loads(f.read())
         f.close()
@@ -22,66 +23,85 @@ if __name__ == "__main__":
 
     # 写入gallery index.md文件的标题
     gallery_index_file = open(str(gallery_index), 'w', encoding='utf-8')
-    gallery_content = "### [<-返回首页]({0}) \n  \n## {1}  \n  \n".format(config['URL1'], config['title'])
+    gallery_content = "### [<-返回首页]({0}index.md) \n  \n## {1}  \n  \n".format(config['ServerURL'], config['title'])
 
-    new_content = None
+    new_content = ''
 
-    if config['Upload_Place'].lower() == "ftp":
-        conn = FtpOp.connect(host=config['FTP'], port=21, username=config['User'], password=config['Password'])
-        current_folder = conn.pwd()
+    p = Pool(config['Pool size'])  # 打开进程池
+    # 将图片的URL写入到gallery的index.md文件中
+    for pic in allimgfiles:
+        # 写入URL
+        gallery_content = gallery_content + f"### {pic.name}  \n![{pic.name}]({config['PicURL']}{config['gallery']}/{pic.name})  \n  \n"
+        if config['Compress']:
+            p.apply_async(CompressImage.compress, (pic.absolute(), config['pic_target'] + "/" + pic.name, config['Compress Method'], config['Resize ratio'], config['Compress quality']), )  # 多进程入
+            # CompressImage.compress(pic.absolute(), config['pic_target'] + "/" + pic.name, config['Compress Method'], config['Resize ratio'], config['Compress quality'])
 
-        # 测试是否成功连接FTP与当前目录是否是相册根目录
-        if current_folder == "/":
-            # 在FTP上建立对应目录并进入, 如果建立不成功,尝试直接进入
-            # 如果能进入不报错, 代表FTP上已经存在该目录
-            try:
-                conn.mkd(config['gallery'])
-            except ftplib.error_perm as ftp_error:
-                print(ftp_error)
-            conn.cwd(config['gallery'])
-            conn.quit()
+    p.close()
+    p.join()
 
-            p = Pool(8)  # 打开进程池
+    # if config['Upload_Place'].lower() == "ftp":
+        # conn = FtpOp.connect(host=config['FTP'], port=21, username=config['User'], password=config['Password'])
+        # current_folder = conn.pwd()
 
-            # 将图片的URL写入到gallery的index.md文件中
-            # 再使用多进程上传图片到FTP上
-            for pic in allimgfiles:
-                # 写入URL
-                gallery_content = gallery_content + "### {0}  \n![{0}]({1}{2}/{0})  \n  \n".format \
-                    (pic.name, config['URL2'], config['gallery'])
-                # FTP_upload(pic, config)
-                p.apply_async(FtpOp.FTP_upload, (pic, config), )  # 多进程入口
-            p.close()
-            p.join()
+        # # 测试是否成功连接FTP与当前目录是否是相册根目录
+        # if current_folder == "/":
+        #     # 在FTP上建立对应目录并进入, 如果建立不成功,尝试直接进入
+        #     # 如果能进入不报错, 代表FTP上已经存在该目录
+        #     try:
+        #         conn.mkd(config['gallery'])
+        #     except ftplib.error_perm as ftp_error:
+        #         print(ftp_error)
+        #     conn.cwd(config['gallery'])
+        #     conn.quit()
 
-        new_content = "### [{0}]({1}{2}/) <- 点击文字打开  \n".format(config['title'], config['URL1'], config['gallery']) \
-                      + "  \n" + "![{0}]({1}{2}/{0})".format(allimgfiles[-1].name, config['URL2'],
-                                                             config['gallery']) + "  \n  \n"
 
-    elif config['Upload_Place'].lower() == "github":
-        p = Pool(8)  # 打开进程池
 
-        # 拼接好相片的URL
-        # 再将对应的相片复制到git仓库里的images文件下
-        for pic in allimgfiles:
-            # 写入URL
-            gallery_content = gallery_content + \
-                              "### {0}  \n![{0}]({1}/{0})  \n  \n".format(pic.name, config['URL3'])
-            # GithubOp.img_process(pic, config)
-            p.apply_async(GithubOp.img_process, (pic, config), )  # 多进程入口
-        p.close()
-        p.join()
+        # p = Pool(3)  # 打开进程池
 
-        for pic in allimgfiles:  # 由于有可能需要多进程去压缩图片, 而git对多进程支持不好,所以处理好图片之后再stage它们
-            cmd = "git add ./images/{0}".format(pic.name)
-            print("Now executing command: {0}".format(cmd))
-            os.system(cmd)
+        # 将图片的URL写入到gallery的index.md文件中
+        # 再使用多进程上传图片到FTP上
+        # for pic in allimgfiles:
+        #     # 写入URL
+        #     gallery_content = gallery_content + "### {0}  \n![{0}]({1}{2}/{0})  \n  \n".format \
+        #         (pic.name, config['PicURL'], config['gallery'])
+            # FTP_upload(pic, config)
 
-        new_content = "### [{0}]({1}{2}/) <- 点击文字打开  \n".format(config['title'], config['URL1'], config['gallery']) \
-                      + "  \n" + "![{0}]({1}/{0})".format(allimgfiles[-1].name, config['URL3']) + "  \n  \n"
+            # if config['Compress']:
+            #     p.apply_async(GithubOp.img_process, (pic, config), )  # 多进程入口
+        #     p.apply_async(CompressImage.compress, (pic, desc_file, 'm', size / 100, quality), )  # 多进程入
+        #
+        # p.close()
+        # p.join()
+
+        # new_content = "### [{0}]({1}{2}/index.md) <- 点击文字打开  \n".format(config['title'], config['ServerURL'], config['gallery']) \
+        #               + "  \n" + "![{0}]({1}{2}/{0})".format(allimgfiles[-1].name, config['PicURL'],
+        #                                                      config['gallery']) + "  \n  \n"
+
+    # elif config['Upload_Place'].lower() == "gitee":
+    #     p = Pool(8)  # 打开进程池
+
+    #     # 拼接好相片的URL
+    #     # 再将对应的相片复制到git仓库里的images文件下
+    #     for pic in allimgfiles:
+    #         # 写入URL
+    #         gallery_content = gallery_content + \
+    #                           "### {0}  \n![{0}]({1}/{0})  \n  \n".format(pic.name, config['URL3'])
+    #         # GithubOp.img_process(pic, config)
+    #         p.apply_async(GithubOp.img_process, (pic, config), )  # 多进程入口
+    #     p.close()
+    #     p.join()
+
+    #     for pic in allimgfiles:  # 由于有可能需要多进程去压缩图片, 而git对多进程支持不好,所以处理好图片之后再stage它们
+    #         cmd = "git add ./images/{0}".format(pic.name)
+    #         print("Now executing command: {0}".format(cmd))
+    #         os.system(cmd)
+
+    #     new_content = "### [{0}]({1}{2}/) <- 点击文字打开  \n".format(config['title'], config['URL1'], config['gallery']) \
+    #                   + "  \n" + "![{0}]({1}/{0})".format(allimgfiles[-1].name, config['URL3']) + "  \n  \n"
 
     # 将相册的相片连接写入相册的index.md
-    gallery_index_file.writelines(gallery_content)
+    new_content = f"### [{config['title']}]({config['ServerURL']}{config['gallery']}/index.md) <- 点击文字打开  \n" + "  \n" + f"![{allimgfiles[-1].name}]({config['PicURL']}{config['gallery']}/{allimgfiles[-1].name})" + "  \n  \n"
+    gallery_index_file.write(gallery_content)
     gallery_index_file.close()
 
     # 将新相册的URL写入首页的index.md
@@ -92,7 +112,8 @@ if __name__ == "__main__":
 
     # 将新相册的内容放到相册中的第一位
     content = old_content[:9] + "  \n" + new_content + old_content[9:]
-    photobook_index_file.writelines(content)
+    photobook_index_file.write(content)
     photobook_index_file.close()
 
-    GithubOp.github_upload(config['gallery'])  # 最后将编辑完成的文件commit与push到github上
+    # GithubOp.github_upload(config['gallery'])  # 最后将编辑完成的文件commit与push到github上
+    print("Totally used: ", int(time.time() - total_time))
--- upload_utils.py
+++ upload_utils.py
@@ -7,40 +7,61 @@ import tempfile
 import time
 from pathlib import Path
 
-# import pyguetzli
+import cv2
+import pyguetzli
+import mozjpeg_lossless_optimization
+from PIL import Image
 
 
-# class CompressImage:
-#
-#     @staticmethod
-#     def compress(pic_path: Path, quality: int = 85):
-#         """
-#         使用Google的guetzli库来压缩图片, 注意这个库只支持jpeg并且比较慢
-#         :param pic_path: 相片的Path对象实例
-#         :param quality: 图片压缩质量
-#         :return: 压缩后的相片的二进制文件内容与相片的路径
-#         """
-#         try:
-#             fb = open(str(pic_path), 'rb').read()  # 压缩图片需要二进制数据
-#             if pic_path.suffix.lower() == ".jpg":  # 只支持jpeg
-#                 print("Reducing pic {0} size...".format(pic_path.name))
-#                 optimized_jpeg = pyguetzli.process_jpeg_bytes(fb, quality=quality)  # 将图片压缩后获得新的二进制数据
-#             else:
-#                 optimized_jpeg = fb
-#             temp = tempfile.mktemp()  # 为了不影响原相片, 压缩后的相片会先放在临时文件
-#             tf = open(temp, "wb+")
-#             tf.write(optimized_jpeg)  # 将压缩后的二进制数据写入临时文件
-#             tf.close()
-#             tf = open(temp, "rb")  # 由于缓存机制的原因, 先保存再读取才能读取到更新后的内容
-#             return tf, temp  # 由于FTP需要文件内容本身, 而上传github只需要文件的路径, 所以这里返回两部分内容
-#         except Exception as msg:
-#             print(msg)
-#             # raise Exception(msg)
+class CompressImage:
+
+    @staticmethod
+    def compress(pic_path: str, target_path: str, chinese=False, method: str = 'g', size: float = 0.5, quality: int = 50):
+        """
+        使用Google的guetzli库来压缩图片, 注意这个库只支持jpeg并且比较慢
+        :param pic_path: 相片的Path对象实例
+        :param target_path: 相片的Path对象实例
+        :param quality: 图片压缩质量
+        :return:
+        """
+        try:
+            print("Reducing pic {0} size...".format(pic_path))
+            begin_time = time.time()
+            # print("path: ", pic_path)
+            if chinese:
+                img = Image.open(pic_path)
+                (width, height) = img.size
+                resize_img = img.resize((int(width * size), int(height * size)))
+                resize_img.save(target_path, quality=quality)
+            else:
+                pic = cv2.imread(pic_path)
+                height, width = pic.shape[:2]  # 获取原始分辨率
+                resize_pic = cv2.resize(pic, (int(width * size), int(height * size)))  # 缩小图片
+                cv2.imwrite(target_path, resize_pic, [int(cv2.IMWRITE_JPEG_QUALITY), quality])  # 按照质量设置保存图片
+            # print("Reduce pic by opencv, used: ".format(pic_path), int(time.time() - begin_time))
+            if method:  # 设置是否需要额外压缩
+                rb = open(str(target_path), 'rb').read()  # 压缩图片需要二进制数据
+                if method == "g":  # 使用google pyguetzli压缩, 效果大概可以压缩到原图1/4, 已经缩小并降低质量的图都要1分钟左右
+
+                    optimized_jpeg = pyguetzli.process_jpeg_bytes(rb, quality=quality)  # 将图片压缩后获得新的二进制数据
+
+                elif method == "m":  # mozila的压缩方法压缩率低, 只能大约压缩5%的体积 但快, 基本不用额外时间
+
+                    optimized_jpeg = mozjpeg_lossless_optimization.optimize(rb)
+
+                    # rb.close() 二进制读取不能close
+                wb = open(str(target_path), 'wb')
+                wb.write(optimized_jpeg)
+                # wb.close()
+            print("pic {0} totally used: ".format(pic_path), int(time.time() - begin_time))
+        except Exception as msg:
+            print(msg)
+            # raise Exception(msg)
 
 
 class FtpOp:
     @staticmethod
-    def connect(host: str = "localhost", ftps: bool = True, port: int = 21, username: str = None, password: str = None):
+    def connect(host: str = "localhost", ftps: bool = True, port: int = 21, username: str = '', password: str = ''):
         """
         使用给定的用户名与密码连接FTP
         :param host: 服务器名或者IP, 注意这里要填写不带FTP://的
@@ -73,7 +94,8 @@ class FtpOp:
         # print(pic_path)
         try:
             if config['Compress']:  # 如果图形需要压缩
-                tf, tp = CompressImage.compress(pic_path)
+                # tf, tp = CompressImage.compress(pic_path)
+                tf = open(str(pic_path), "rb")
             else:
                 tf = open(str(pic_path), "rb")
 
@@ -103,8 +125,9 @@ class GithubOp:
         try:
             pic_name = pic_path.name
             if config['Compress'] is True:  # 如果图形需要压缩
-                pic_file, pic_path = CompressImage.compress(pic_path, quality=70)
-                pic_file.close()
+                pic_path = CompressImage.compress(pic_path, quality=70)
+                # pic_file.close()
+                # pass
             img_path = Path("./images/{0}".format(pic_name))
             shutil.copy(str(pic_path), str(img_path))
             time.sleep(random.randint(1, 3))
@@ -115,7 +138,7 @@ class GithubOp:
     @staticmethod
     def github_upload(gallery: str):
         """
-        将已经stage人内容commit与push到github
+        将已经stage的内容commit与push到github
         :param gallery: 要上传的相册名称
         :return: 无
         """
